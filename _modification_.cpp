f27d65a3484935b84729805d7b6a4fcf84f1037e
Hyperion <kvfromtg@gmail.com>
2023-03-12 13:53:26 +0700

    Finish create new school year function

--- _modification_.cpp
+++ _modification_.cpp
@@ -18,3 +18,30 @@ void addStringNode(stringNode *&head, string data) {
     temp = curr = nullptr;
 }
 
+void createSchoolYear(schoolYear &object, string name) {
+    //check valid year
+
+    //
+    if (name.length() < 5) {
+        string temp = name;
+        temp[3] += 1;
+        name = name + "-" + temp;
+    }
+    if (mkdir(("Data/" + name).c_str()) == -1) {
+        cout << "This school year has been created before" << endl;
+        return;
+    }
+    cout << "Created school year " << name << endl; 
+    if (mkdir(("Data/"+name+"/Classes").c_str()) == -1) {
+        cout << "Error while creating Class folder for " << name << endl;   //for debugging purpose
+        return;
+    }
+    object.SY = name;
+    object.classes = nullptr;   //add first node using function
+    object.semester = nullptr;  //add first node using function
+    ofstream out_file {"Data/schoolYears.txt", std::ios::app};
+    out_file << name << endl;
+
+
+}
+
