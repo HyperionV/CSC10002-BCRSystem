3fdeb68e3c55c3eb73bc23d9ec0a5dd6ab68247e
Hyperion <kvfromtg@gmail.com>
2023-03-12 13:52:18 +0700

    Write prototype for some basic functions

---
+++ _modification_.h
@@ -0,0 +1,6 @@
+#include "_struct_temp.h"
+
+void addStringNode(stringNode *&head, string data);
+void createSchoolYear(schoolYear &object, string name);
+void createSemester(schoolYear &object);
+void createClasses(schoolYear &object);
