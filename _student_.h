5f0a85bc7caebf50e18d1b5357aa05ab7541e327
Dtt234 <127120317+Dtt234@users.noreply.github.com>
2023-03-12 14:50:59 +0700

    basic template student

---
+++ _student_.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+#include <iostream>
+#include "_struct_temp.h"
+
+void getStudentData(student& A);
+void printDate(date d);
+void menuStudent(student A);
+void viewScoreboard(student A);
+void viewCourse(student A);
+void viewProfile(student A);
+void changePassStudent(student &A);
+#endif
