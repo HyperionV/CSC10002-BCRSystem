e22b529cd96b7aacbff63d60997926ac5e13678d
Dtt234 <127120317+Dtt234@users.noreply.github.com>
2023-03-14 23:57:33 +0700

    getting student data from file

---
+++ _student_h.h
@@ -0,0 +1,17 @@
+#pragma once
+#pragma once
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "_struct_temp.h"
+
+int findNthOccurrence(std::string s, char x, int count);
+student getStudentData(std::string id, std::string pathStudent);
+void menuStudent(student A);
+void viewScoreboard(student A);
+void viewCourse(student A);
+void viewProfile(student A);
+void changePassStudent(student& A);
+#endif
