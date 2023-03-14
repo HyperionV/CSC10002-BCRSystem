54f3143c69d06a009421deda07e1c5d4a65298b9
Hyperion <kvfromtg@gmail.com>
2023-03-14 08:15:29 +0700

    Fix some samll bugs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include "_modification_.cpp"
 
 int main() {
-    
+    schoolYear SC;
+    createSchoolYear(SC, "2022");
+    createClass(SC);
+    semester sem;
+    createSemester(sem);
+    createCourse(sem);
 }
