674c5b03800268da0996d78a747734fde347ad36
vongagh094 <johnysiu2004@gmail.com>
2023-03-17 20:29:15 +0700

    staff functions

---
+++ newest/main.cpp
@@ -0,0 +1,12 @@
+#include "_modification_.cpp"
+
+int main() {
+    schoolYear SC;
+    semester sem;
+    student temp;
+    // viewStudentScoreboard("2020-2021", "22CLC06" , "22127158");
+    // viewCourseScoreBoard("CSC101" , "2020-2021", "B");
+    // viewStudentInCourse("CSC101" , "2020-2021", "B");
+    // delStudentInCourse("CSC101" , "2020-2021", "B");
+    updateCourseScoreboard("CSC101" , "2020-2021", "B");
+}
