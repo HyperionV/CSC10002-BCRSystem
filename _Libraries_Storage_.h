12549fc4c9c75bcd3f0976d33fd8768b767739f5
VuLeTrongVan20CLC06 <119859213+VuLeTrongVan20CLC06@users.noreply.github.com>
2023-04-12 21:34:24 +0700

    Upload Menu Version2: New Libraries Storage File

---
+++ _Libraries_Storage_.h
@@ -0,0 +1,26 @@
+#ifndef __LIBRARIES_STORAGE_H_
+#define __LIBRARIES_STORAGE_H_
+
+// Including some libraries to manipulate/control everything about Input/Output Formatting
+#include <ios>
+#include <iomanip>
+#include <iostream>
+
+// Including some libraries to manipulate/control everything about String or an array of characters or single character
+#include <string>
+#include <cctype>
+#include <cstring>
+
+// Including library to manipulate/control everything about Window,....
+#include <dos.h>
+#include <conio.h>
+#include <Windows.h>
+
+// Including some libraries to manipulate/control everything about File, Folder, ...
+#include <fstream>
+#include <sys/stat.h>
+#include <msclr/marshal_cppstd.h>
+#include <list>
+#include <map>
+
+#endif // !__LIBRARIES_STORAGE_H_
